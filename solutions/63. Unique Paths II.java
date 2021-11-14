f0a87c3845f9fdd3890be05cc689141682b18295
Sidhant Bansal <sidhantbansal1997@gmail.com>
2021-11-13 23:11:50 -0800

    0 ms

---
+++ solutions/63. Unique Paths II.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+        if(obstacleGrid[0][0] == 1) return 0;
+        obstacleGrid[0][0] = 1;
+        
+        
+        for(int i = 1; i < col ; i++){    
+            //Main Condition
+            if(obstacleGrid[0][i] == 0 && obstacleGrid[0][i-1] == 1){
+                obstacleGrid[0][i] = 1;
+            }
+            else{
+                obstacleGrid[0][i] = 0;
+            }
+        }
+        
+        for(int i = 1; i < row ; i++){
+            if(obstacleGrid[i][0] == 0 && obstacleGrid[i-1][0] == 1){
+                obstacleGrid[i][0] = 1;
+            }
+            else{
+                obstacleGrid[i][0] = 0;
+            }
+        }
+        
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < col; j++){
+                if(obstacleGrid[i][j] == 1){
+                    obstacleGrid[i][j] = 0;
+                }
+                else{
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+                }
+            }
+        }
+        
+        return obstacleGrid[row-1][col-1];
+    }
+}
