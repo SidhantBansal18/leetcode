2d73c5921ceef2fe850c0a8edc11e4d3a1cda0b6
Sidhant Bansal <sidhantbansal1997@gmail.com>
2021-11-20 16:41:05 -0800

    2 ms

--- solutions/986. Interval List Intersections.java
+++ solutions/986. Interval List Intersections.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        List<List<Integer>> result = new ArrayList();
+        //List<List<Integer>> result = new ArrayList();
+        List<int[]> res = new ArrayList();
         if(firstList.length == 0 || secondList.length == 0) return new int[0][0];
         
         int start1 = 0;
@@ -13,7 +14,8 @@ class Solution {
                 int end = Math.min(firstList[start1][1], secondList[start2][1]);
                 //smallResult.add(start);
                 //smallResult.add(end);
-                result.add(Arrays.asList(start,end));
+                //result.add(Arrays.asList(start,end));
+                res.add(new int[]{start,end});
             }
             if(firstList[start1][1] > secondList[start2][1]){
                 start2++;
@@ -23,12 +25,12 @@ class Solution {
             }
         }
         
-        int[][] res = new int[result.size()][2];
-        for(int i = 0; i < res.length; i++){
-            for(int j = 0; j < 2; j++){
-                res[i][j] = result.get(i).get(j);
-            }
-        }
-        return res;
+        // int[][] res = new int[result.size()][2];
+        // for(int i = 0; i < res.length; i++){
+        //     for(int j = 0; j < 2; j++){
+        //         res[i][j] = result.get(i).get(j);
+        //     }
+        // }
+        return res.toArray(new int[res.size()][]);
     }
 }
