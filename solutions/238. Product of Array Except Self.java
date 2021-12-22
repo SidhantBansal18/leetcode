9737b5f6355afde0062afc62c477b695c4842eee
Sidhant Bansal <sidhantbansal1997@gmail.com>
2021-12-22 00:46:31 -0800

    1 ms

--- solutions/238. Product of Array Except Self.java
+++ solutions/238. Product of Array Except Self.java
@@ -1,26 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         
+        //O(1) space approach
         int len = nums.length;
         
         int[] leftProd = new int[len];
-        int[] rightProd = new int[len];
-        int[] result = new int[len];
         
         leftProd[0] = 1;
-        for(int i = 1 ; i < len; i++){
+        for(int i = 1; i < len; i++){
             leftProd[i] = leftProd[i-1] * nums[i-1];
         }
         
-        rightProd[len-1] = 1;
-        for(int i = len-2; i >=0 ; i--){
-            rightProd[i] = rightProd[i+1] * nums[i+1];
+        int tempRes = 1;
+        for(int i = len-1; i >=0; i--){
+            leftProd[i] = tempRes * leftProd[i];
+            tempRes *= nums[i];
         }
         
-        for(int i = 0; i < len; i++){
-            result[i] = leftProd[i] * rightProd[i];
-        }
-        
-        return result;
+        return leftProd;
     }
 }
