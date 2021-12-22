3aae08ce6b6ff51ef810d49fe7b7913d7678090e
Sidhant Bansal <sidhantbansal1997@gmail.com>
2021-12-22 00:09:19 -0800

    2 ms

--- solutions/334. Increasing Triplet Subsequence.java
+++ solutions/334. Increasing Triplet Subsequence.java
@@ -1,27 +1,18 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int start = 0;
-        int end = nums.length-1;
         
-        int[] leftMin = new int[nums.length]; // Left min of every element
-        
-        int[] rightMax = new int[nums.length]; // Right max of every element
-        
-        leftMin[0] = Integer.MAX_VALUE;
-        for(int i = 1; i < leftMin.length; i++){
-            leftMin[i] = Math.min(leftMin[i-1], nums[i-1]);
+        //O(1) space approach
+        if(nums == null || nums.length <= 2){
+            return false;
         }
         
-        rightMax[rightMax.length-1] = Integer.MIN_VALUE;
-        for(int i = rightMax.length-2; i >= 0; i--){
-            rightMax[i] = Math.max(rightMax[i+1], nums[i+1]);
+        int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE;
+        for(int num: nums){
+            if(num < a) a = num;
+            else if(num > a && num < b) b = num;
+            else if (num > b) return true;
         }
         
-        for(int i = 1; i < nums.length-1; i++){
-            if(nums[i] > leftMin[i] && nums[i] < rightMax[i]){
-                return true;
-            }
-        }
         return false;
     }
 }
