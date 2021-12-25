02fca253414deb4b2bdfb736fb2b107aac56f1bd
Sidhant Bansal <sidhantbansal1997@gmail.com>
2021-12-24 21:38:27 -0800

    0 ms

--- solutions/70. Climbing Stairs.java
+++ solutions/70. Climbing Stairs.java
@@ -3,10 +3,15 @@ class Solution {
         return helper(n, new int[n+1]);
     }
     
-    //Top down approach
+    //Correct Top down approach
     public int helper(int n, int[] memo){
-        if(n == 1)  return 1;
-        if(n == 2)  return 2;
+        
+        if(n > 0 && n < 3){
+            if(memo[n] == 0){
+                memo[n] = n;
+            }
+            return memo[n];
+        }
         
         if(memo[n] == 0){
             memo[n] = helper(n-1, memo) + helper(n-2, memo);
