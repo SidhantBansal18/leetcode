84429f004a27312c53acfe5d8d1d0a18a7f7afe4
Sidhant Bansal <sidhantbansal1997@gmail.com>
2021-12-25 20:00:17 -0800

    1 ms

--- solutions/62. Unique Paths.java
+++ solutions/62. Unique Paths.java
@@ -1,26 +1,38 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
         
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(i == 0 && j == 0){
-                    dp[i][j] = 0;
-                }
-                
-                if(i == 0){
-                    dp[i][j] = 1;
-                }
-                
-                if(j == 0){
-                    dp[i][j] = 1;
-                }
-                else if( i != 0 && j != 0){
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
-            }
+        if(m == 1 && n == 1){
+            return 1;
         }
         
-        return dp[m-1][n-1];
+        int[][] memo = new int[m+1][n+1];
+        
+        for(int[] row: memo){
+            Arrays.fill(row, -1);
+        }
+        
+        return helper(m-1,n-1, memo);
+    }
+    
+    //My top down approach
+    
+    public int helper(int row, int column, int[][] memo){
+        
+        if(row < 0 || column < 0){
+            return 0;
+        }
+        
+        if((row == 1 && column == 0) || (row == 0 && column == 1)){
+            if(memo[row][column] == -1){
+                memo[row][column] = 1;
+            }
+            return memo[row][column];
+        }
+        
+        if(memo[row][column] == -1){
+            memo[row][column] = helper(row-1,column, memo) + helper(row,column-1, memo);
+        }
+        
+        return memo[row][column];
     }
 }
