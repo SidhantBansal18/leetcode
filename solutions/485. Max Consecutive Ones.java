37ba14a08914bd830a5dc50f9ab5b6987dc54447
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-01-16 21:22:11 -0800

    3 ms

--- solutions/485. Max Consecutive Ones.java
+++ solutions/485. Max Consecutive Ones.java
@@ -1,20 +1,27 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         
-        int max = 0;
-        int consecutiveMax = 0;
+        int result = Integer.MIN_VALUE;
         
         for(int i = 0; i < nums.length; i++){
+            
             if(nums[i] == 1){
-                consecutiveMax += 1;
+                int j = i;
+                int res = 0;
+                
+                while(j < nums.length && nums[j] == 1){
+                    res++;
+                    j++;
+                }
+                
+                result = Math.max(result, res);
             }
             
-            else{
-                max = Math.max(max, consecutiveMax);
-                consecutiveMax = 0;
-            }
         }
         
-        return Math.max(max, consecutiveMax);
+        if(result == Integer.MIN_VALUE){
+            return 0;
+        }
+        return result;
     }
 }
