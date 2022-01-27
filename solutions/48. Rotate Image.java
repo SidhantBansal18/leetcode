e8683c885af01c14fb5af23a3b1f4ee8356bebcc
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-01-26 20:45:33 -0800

    0 ms

--- solutions/48. Rotate Image.java
+++ solutions/48. Rotate Image.java
@@ -1,34 +1,37 @@
 class Solution {
     public void rotate(int[][] matrix) {
+        
         transpose(matrix);
-        swap(matrix);
+        rotate(matrix,0,matrix[0].length-1);
     }
     
     public void transpose(int[][] matrix){
         
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
+                
                 if(i != j && i < j){
                     int temp = matrix[i][j];
                     matrix[i][j] = matrix[j][i];
                     matrix[j][i] = temp;
                 }
+                
             }
         }
     }
     
-    public void swap(int[][] arr){
-        int start = 0;
-        int end = arr.length-1;
+    public void rotate(int[][] matrix, int start, int end){
         
         while(start < end){
-            for(int i = 0; i < arr.length; i++){
-                int temp = arr[i][start];
-                arr[i][start] = arr[i][end];
-                arr[i][end] = temp;
+            
+            for(int i = 0; i < matrix[0].length; i++){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
             }
             start++;
             end--;
         }
+        
     }
 }
