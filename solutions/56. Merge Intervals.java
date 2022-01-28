6d55a6536c6ba462ed4ccabb4cf9f4ff5e03ba01
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-01-28 00:37:06 -0800

    N/A

--- solutions/56. Merge Intervals.java
+++ solutions/56. Merge Intervals.java
@@ -1,36 +1,38 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+     
+        if(intervals.length == 1)   return intervals;
         
-        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         
-        List<List<Integer>> resultList = new ArrayList();
+        List<List<Integer>> res = new ArrayList();
         
-        int current = 0;
-        resultList.add(Arrays.asList(intervals[0][0], intervals[0][1]));
+        res.add(Arrays.asList(intervals[0][0], intervals[0][1]));
         
         int index = 1;
+        int current = 0;
         
         while(index < intervals.length){
             
-            if(resultList.get(current).get(1) >= intervals[index][0]){
-                resultList.get(current).set(1, Math.max( resultList.get(current).get(1), intervals[index][1]));         
+            if(res.get(current).get(1) >= intervals[index][0]){
+                res.get(current).set(1, Math.max(res.get(current).get(1), intervals[index][1]));
             }
             else{
                 List<Integer> list = new ArrayList();
                 list.add(intervals[index][0]);
                 list.add(intervals[index][1]);
-                resultList.add(list);
+                res.add(list);
                 current++;
             }
             index++;
             
         }
         
-        int[][] result = new int[resultList.size()][2];
+        int[][] result = new int[res.size()][2];
         
-        for(int i = 0 ; i < result.length; i++){
-            result[i][0] = resultList.get(i).get(0);
-            result[i][1] = resultList.get(i).get(1);
+        for(int i = 0; i < result.length; i++){
+            result[i][0] = res.get(i).get(0);
+            result[i][1] = res.get(i).get(1);
         }
         
         return result;
