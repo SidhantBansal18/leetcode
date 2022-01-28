68d4afc86aa4d9e0297e365e0a0acb588c580d28
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-01-28 14:30:16 -0800

    0 ms

--- solutions/62. Unique Paths.java
+++ solutions/62. Unique Paths.java
@@ -1,29 +1,22 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-  
-        int[][] memo = new int[m+1][n+1];
         
-        for (int i = 0; i < m; ++i) { // init
-            for (int j = 0; j < n; ++j) {
-                memo[i][j] = -1;
+        int[][] dp = new int[m][n];
+        
+        for(int i = 0; i < dp[0].length; i++){
+            dp[0][i] = 1;
+        }
+        
+        for(int i = 0; i < dp.length; i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[0].length; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         
-        return helper(m-1,n-1, memo);
-    }
-    
-    //My Top down approach after removing Arrays.fill() at line 12 and handled base cases in helper function only;
-    
-    public int helper(int row, int column, int[][] memo){
-        
-        if(row == 0 || column == 0){
-            return 1;
-        }
-        
-        if(memo[row][column] == -1){
-            memo[row][column] = helper(row-1,column, memo) + helper(row,column-1, memo);
-        }
-        
-        return memo[row][column];
+        return dp[m-1][n-1];
     }
 }
