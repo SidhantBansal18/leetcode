8600634fb891b6a42c75f0cab31ab21966283bba
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-02-07 21:44:44 -0800

    5 ms

--- solutions/200. Number of Islands.java
+++ solutions/200. Number of Islands.java
@@ -1,13 +1,10 @@
 class Solution {
     public int numIslands(char[][] grid) {
         
-        int row = grid.length;
-        int col = grid[0].length;
-        
         int result = 0;
         
-        for(int i = 0; i < row; i++){
-            for(int j = 0;  j < col; j++){
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
                     helper(grid, i, j);
                     result += 1;
@@ -16,23 +13,20 @@ class Solution {
         }
         
         return result;
+        
     }
     
-    public void helper(char[][] grid, int row, int col){
+    public void helper(char[][] grid, int x, int y){
         
-        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0'){
+        if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] != '1'){
             return;
         }
         
-        
-        
-        grid[row][col] = '0';
-        
-        helper(grid, row+1, col);
-        helper(grid,row-1, col);
-        helper(grid,row, col+1);
-        helper(grid,row, col-1);
-        
+        grid[x][y] = '0';
+        helper(grid, x+1, y);
+        helper(grid, x, y+1);
+        helper(grid, x-1, y);
+        helper(grid, x, y-1);
         return;
     }
 }
