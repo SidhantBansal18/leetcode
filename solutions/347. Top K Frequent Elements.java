d3f46b5d69faac02c7c68ba2090ae71d2d2de856
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-02-15 01:27:14 -0800

    10 ms

--- solutions/347. Top K Frequent Elements.java
+++ solutions/347. Top K Frequent Elements.java
@@ -1,22 +1,28 @@
 class Solution {
+    
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap();
-        for(int i : nums){
-            map.put(i, map.getOrDefault(i,0) + 1);
+        
+        HashMap<Integer, Integer> map = new HashMap();
+        
+        for (int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
         
-        Queue<Integer> queue = new PriorityQueue<>((n1,n2) -> map.get(n1) - map.get(n2));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
+        
         for(int i : map.keySet()){
-            queue.offer(i);
-            if(queue.size() > k){
-                queue.poll();
+            pq.offer(i);
+            if(pq.size() > k){
+                pq.poll();
             }
         }
         
-        int[] res = new int[k];
-        for(int i = 0; i < k; i++){
-            res[i] = queue.poll();
+        int[] ans = new int[pq.size()];
+        
+        for(int i = 0; i < ans.length; i++){
+            ans[i] = pq.poll();
         }
-        return res;
+        
+        return ans;
     }
 }
