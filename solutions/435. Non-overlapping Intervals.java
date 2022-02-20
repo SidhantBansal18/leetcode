d9e43887482462b848569f0eb96bb84170ea3e6f
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-02-20 00:51:28 -0800

    50 ms

--- solutions/435. Non-overlapping Intervals.java
+++ solutions/435. Non-overlapping Intervals.java
@@ -1,23 +1,21 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        //Arrays.sort(intervals, (arr1, arr2) -> arr1[0]-arr2[0]);
-        Arrays.sort(intervals, (arr1, arr2) -> arr1[1]-arr2[1]);
         
-        List<List<Integer>> result = new ArrayList();
-        result.add(Arrays.asList(intervals[0][0], intervals[0][1]));
-        int index = 0;
-        int toRemove = 0;
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+        int result = 0;
+        int prevEnd = intervals[0][1];
         
         for(int i = 1; i < intervals.length; i++){
-            if(intervals[i][0] >= result.get(index).get(1)){
-                result.add(Arrays.asList(intervals[i][0], intervals[i][1]));
-                index++;
+            
+            if(intervals[i][0] >= prevEnd){
+                prevEnd = intervals[i][1];
             }
             else{
-                toRemove++;
+                result++;
+                prevEnd = Math.min(prevEnd, intervals[i][1]);
             }
         }
         
-        return toRemove;
-    }
+        return result;
+    }
 }
