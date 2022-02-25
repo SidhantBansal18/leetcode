8c3989a7d5b25f4f73704c507d2806503735c6b8
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-02-24 17:16:02 -0800

    7 ms

--- solutions/39. Combination Sum.java
+++ solutions/39. Combination Sum.java
@@ -1,31 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList();
-        List<Integer> temp = new ArrayList();
-        helper(0, result, temp ,candidates, target);
-        return result;
         
+        List<Integer> temp = new ArrayList();
+        
+        helper(result, temp, candidates, target, 0);
+        
+        return result;
     }
     
-    public void helper(int start,List<List<Integer>> result, List<Integer> temp ,int[] candidates ,int remaining){
-        
-        if(start > candidates.length && remaining != 0){
+    public void helper(List<List<Integer>> result, List<Integer> temp, int[] candidates, int target, int start){
+        if(target == 0){
+            result.add(new ArrayList<>(temp));
             return;
         }
         
-        if(remaining == 0){
-            result.add(new ArrayList(temp));
-            return;
-        }
-        
-        if(remaining < 0){
+        if(target < 0){
             return;
         }
         
         for(int i = start; i < candidates.length; i++){
             temp.add(candidates[i]);
-            helper(i, result, temp, candidates, remaining-candidates[i]);
+            helper(result, temp, candidates, target-candidates[i], i);
             temp.remove(temp.size()-1);
         }
     }
