d556dd8c8c5e107dbaf51fa101125da96fd48c27
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-02-26 20:24:55 -0800

    1 ms

--- solutions/53. Maximum Subarray.java
+++ solutions/53. Maximum Subarray.java
@@ -1,16 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        
         int sum = nums[0];
         int maxSum = nums[0];
         
         for(int i = 1; i < nums.length; i++){
-            
-            sum = Math.max(sum+nums[i], nums[i]);
-            
+            if(sum < 0){
+                sum = nums[i];
+            }
+            else{
+                sum += nums[i];
+            }
             maxSum = Math.max(sum, maxSum);
-            
         }
-        
         return maxSum;
     }
 }
