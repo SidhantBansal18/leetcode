4fecb08035a2a9ec31168ce556217358ec788ebc
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-01 00:13:47 -0800

    0 ms

--- solutions/100. Same Tree.java
+++ solutions/100. Same Tree.java
@@ -18,12 +18,9 @@ class Solution {
         
         if(p == null && q == null)  return true;
         
-        if(p == null || q == null)  return false;
+        if((p == null || q == null) || p.val != q.val)  return false;
         
-        if(p.val == q.val){
-            return isSameTree(p.left, q.left) && isSameTree(p.right,q.right);
-        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         
-        return false;
     }
 }
