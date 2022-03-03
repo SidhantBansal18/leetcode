59a7c63128f6fcce85b401a2eb22f3ba0188b608
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-02 23:45:04 -0800

    0 ms

--- solutions/226. Invert Binary Tree.java
+++ solutions/226. Invert Binary Tree.java
@@ -16,7 +16,9 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         
-        if(root == null)    return null;
+        if(root == null){
+            return null;
+        }
         
         TreeNode temp = root.left;
         root.left = invertTree(root.right);
