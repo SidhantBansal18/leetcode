47dde9d95f44e801ce2e9720d54e2c13bfe8cb19
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-04 02:04:59 -0800

    4 ms

--- solutions/1. Two Sum.java
+++ solutions/1. Two Sum.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] res = new int[2];
-        HashMap<Integer, Integer> map = new HashMap();
+        
+        int[] ans = new int[2];
+        
+        Map<Integer, Integer> map = new HashMap();
         
         for(int i = 0; i < nums.length; i++){
-            
             map.put(nums[i], i);
-            
         }
         
         for(int i = 0; i < nums.length; i++){
             int toFind = target - nums[i];
-            
             if(map.containsKey(toFind) && map.get(toFind) != i){
-                res[0] = i;
-                res[1] = map.get(toFind);
-                return res;
+                ans[0] = i;
+                ans[1] = map.get(toFind);
             }
+            
         }
         
-        return res;
+        return ans;
     }
 }
