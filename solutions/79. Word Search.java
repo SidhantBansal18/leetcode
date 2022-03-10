db85791d0925299ca14dbd968f8a066779434418
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-09 17:18:45 -0800

    130 ms

--- solutions/79. Word Search.java
+++ solutions/79. Word Search.java
@@ -1,33 +1,38 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-       
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == word.charAt(0) && dfs(board,i,j, word,0)){
-                     return true;
-                }
+        
+        int row = board.length;
+        int col = board[0].length;
+        
+        for(int i =0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(board[i][j] == word.charAt(0) && isWordThere(board, word, i, j, 0)){
+                    return true;
+                }
             }
         }
         return false;
     }
     
-    public boolean dfs(char[][] board, int i, int j, String word, int index){
+    public boolean isWordThere (char[][] board, String word, int row, int col, int index){
         
         if(index == word.length()){
             return true;
         }
         
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(index)){
+        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != word.charAt(index)){
             return false;
         }
         
-        char temp = board[i][j];
-        board[i][j] = '*';
-        boolean check = dfs(board, i+1, j, word, index+1) ||
-        dfs(board, i-1, j, word, index+1) ||
-        dfs(board, i, j+1, word, index+1) ||
-        dfs(board, i, j-1, word, index+1);
-        board[i][j] = temp;
-        return check;
+        char temp = board[row][col];
+        board[row][col] = '*';
+        
+        boolean result = isWordThere(board, word, row+1, col, index+1) ||
+            isWordThere(board, word, row-1, col, index+1) || 
+            isWordThere(board, word, row, col+1, index+1) ||
+            isWordThere(board, word, row, col-1, index+1);
+        
+        board[row][col] = temp;
+        return result;
     }
 }
