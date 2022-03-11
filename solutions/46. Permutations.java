9aa232905e1f65574c27a077e736117da6809952
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-10 17:55:58 -0800

    4 ms

--- solutions/46. Permutations.java
+++ solutions/46. Permutations.java
@@ -7,12 +7,13 @@ class Solution {
         
         List<Integer> temp = new ArrayList();
         
-        backtrack(nums,result,temp);
+        generatePermutations(nums, result, temp);
         
         return result;
+        
     }
     
-    public void backtrack(int[] nums, List<List<Integer>> result, List<Integer> temp){
+    public void generatePermutations(int[] nums, List<List<Integer>> result, List<Integer> temp){
         
         if(temp.size() == nums.length){
             result.add(new ArrayList<>(temp));
@@ -21,7 +22,7 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             if(temp.contains(nums[i]))  continue;
             temp.add(nums[i]);
-            backtrack(nums, result, temp);
+            generatePermutations(nums, result, temp);
             temp.remove(temp.size()-1);
         }
     }
