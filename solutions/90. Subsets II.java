0fecb97d124b81913c0fb7c35381ecfc5959281d
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-10 17:55:49 -0800

    2 ms

--- solutions/90. Subsets II.java
+++ solutions/90. Subsets II.java
@@ -1,22 +1,25 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result = new ArrayList();
+        
         Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList();
+        
         List<Integer> temp = new ArrayList();
         
-        backtrack(nums, result, temp, 0);
+        generateSubsets(nums, result, temp, 0);
         
         return result;
+        
     }
     
-    public void backtrack(int[] nums, List<List<Integer>> result, List<Integer> temp, int start){
+    public void generateSubsets(int[] nums, List<List<Integer>> result, List<Integer> temp, int start){
         
         result.add(new ArrayList<>(temp));
         
         for(int i = start; i < nums.length; i++){
             if(i > start && nums[i] == nums[i-1])   continue;
             temp.add(nums[i]);
-            backtrack(nums, result, temp, i+1);
+            generateSubsets(nums, result, temp, i+1);
             temp.remove(temp.size()-1);
         }
         
