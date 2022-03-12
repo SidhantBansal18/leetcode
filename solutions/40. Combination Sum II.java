3db21d31c4d8d0b36c7e21e364f524edebb38511
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-11 19:55:30 -0800

    8 ms

--- solutions/40. Combination Sum II.java
+++ solutions/40. Combination Sum II.java
@@ -1,29 +1,33 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        
         Arrays.sort(candidates);
+        
         List<List<Integer>> result = new ArrayList();
+        
         List<Integer> temp = new ArrayList();
         
-        helper(0, result, temp, candidates, target);
+        generateCombinationSum(result, temp, candidates, target, 0);
+        
         return result;
+        
     }
     
-    public void helper(int start, List<List<Integer>> result, List<Integer> temp, int[] candidates ,int remaining){
+    public void generateCombinationSum(List<List<Integer>> result, List<Integer> temp, int[] candidates, int target, int index){
         
-        if(start > candidates.length || remaining < 0){
-            return;
-        }
-        
-        if(remaining == 0){
+        if(target == 0){
             result.add(new ArrayList<>(temp));
-            return;
         }
-        
-        for(int i = start; i < candidates.length; i++){
-            if(i > start && candidates[i] == candidates[i-1])   continue;
-            temp.add(candidates[i]);
-            helper(i+1, result, temp, candidates, remaining-candidates[i]);
-            temp.remove(temp.size() - 1);
+        else if(target < 0){
+             return;
         }
+        else{
+            for(int i = index; i < candidates.length; i++){
+                if(i > index && candidates[i] == candidates[i-1])   continue;
+                temp.add(candidates[i]);
+                generateCombinationSum(result, temp, candidates, target-candidates[i], i+1);
+                temp.remove(temp.size()-1);
+            }
+        } 
     }
 }
