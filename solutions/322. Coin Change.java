295a6bc8baad91476c7c9d061013118654b280a7
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-15 00:19:31 -0700

    24 ms

--- solutions/322. Coin Change.java
+++ solutions/322. Coin Change.java
@@ -5,14 +5,14 @@ class Solution {
         Arrays.fill(dp, amount+1);
         dp[0] = 0;
         
-        for(int i = 1; i < amount+1; i++){
+        for(int i = 0; i <= amount; i++){
             for(int j = 0; j < coins.length; j++){
-                if(i - coins[j] >= 0){
-                    dp[i] = Math.min(dp[i], dp[i-coins[j]]+1);
+                if(coins[j] <= i){
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
                 }
             }
         }
-     
-        return dp[amount] != amount +1? dp[amount]: -1;
+        
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
