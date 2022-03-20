f45461bec0cd8bf3257babd509113d4790b4dadd
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-19 22:44:31 -0700

    1 ms

--- solutions/62. Unique Paths.java
+++ solutions/62. Unique Paths.java
@@ -1,22 +1,22 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        int[][] dp = new int[m][n];
+        int[][] arr = new int[m][n];
         
-        for(int i = 0; i < dp[0].length; i++){
-            dp[0][i] = 1;
+        for(int i = 0; i < arr.length; i++){
+            arr[i][arr[0].length-1] = 1;
         }
         
-        for(int i = 0; i < dp.length; i++){
-            dp[i][0] = 1;
+        for(int i = 0; i < arr[0].length; i++){
+            arr[arr.length-1][i] = 1;
         }
         
-        for(int i = 1; i < dp.length; i++){
-            for(int j = 1; j < dp[0].length; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        for(int i = arr.length-2; i >= 0; i--){
+            for(int j = arr[0].length-2; j >=0; j--){
+                arr[i][j] = arr[i+1][j] + arr[i][j+1];
             }
         }
         
-        return dp[m-1][n-1];
+        return arr[0][0];
     }
 }
