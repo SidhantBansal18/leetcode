08729934f72f4fa85935d830e4ab3b386a6f42a6
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-23 02:52:35 -0700

    5 ms

--- solutions/871. Minimum Number of Refueling Stops.java
+++ solutions/871. Minimum Number of Refueling Stops.java
@@ -1,17 +1,17 @@
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
         
-        Queue<Integer> pq = new PriorityQueue<>();
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int index = 0;
         int result;
         
         for(result = 0; startFuel < target; result++){
             
             while(index < stations.length && stations[index][0] <= startFuel){
-                pq.offer(-stations[index++][1]);
+                pq.offer(stations[index++][1]);
             }
             if(pq.isEmpty())    return -1;
-            startFuel += -pq.poll();
+            startFuel += pq.poll();
             
         }
         
