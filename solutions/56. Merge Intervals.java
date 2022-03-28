03702b3c4fff7467e2e78137717eae2c1b4c0853
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-27 20:25:58 -0700

    19 ms

--- solutions/56. Merge Intervals.java
+++ solutions/56. Merge Intervals.java
@@ -1,40 +1,32 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-     
-        if(intervals.length == 1)   return intervals;
         
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
         
-        List<List<Integer>> res = new ArrayList();
+        List<List<Integer>> result = new ArrayList();
         
-        res.add(Arrays.asList(intervals[0][0], intervals[0][1]));
+        result.add(Arrays.asList(intervals[0][0], intervals[0][1]));
         
-        int index = 1;
         int current = 0;
         
-        while(index < intervals.length){
+        for(int i = 1; i < intervals.length; i++){
             
-            if(res.get(current).get(1) >= intervals[index][0]){
-                res.get(current).set(1, Math.max(res.get(current).get(1), intervals[index][1]));
+            if(intervals[i][0] <= result.get(current).get(1)){
+                result.get(current).set(1, Math.max(intervals[i][1], result.get(current).get(1)));
             }
             else{
-                List<Integer> list = new ArrayList();
-                list.add(intervals[index][0]);
-                list.add(intervals[index][1]);
-                res.add(list);
+                result.add(Arrays.asList(intervals[i][0],intervals[i][1]));
                 current++;
             }
-            index++;
-            
         }
         
-        int[][] result = new int[res.size()][2];
+        int[][] resultant = new int[result.size()][2];
         
-        for(int i = 0; i < result.length; i++){
-            result[i][0] = res.get(i).get(0);
-            result[i][1] = res.get(i).get(1);
+        for(int i = 0; i < resultant.length; i++){
+            resultant[i][0] = result.get(i).get(0);
+            resultant[i][1] = result.get(i).get(1);
         }
         
-        return result;
+        return resultant;
     }
 }
