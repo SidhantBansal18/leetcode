ac8730d1a48bd1f6c9cb0f3891a2df71446803e6
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-03-31 17:40:01 -0700

    0 ms

--- solutions/162. Find Peak Element.java
+++ solutions/162. Find Peak Element.java
@@ -7,11 +7,11 @@ class Solution {
         while(left < right){
             int mid = left + (right-left)/2;
             
-            if(nums[mid] > nums[mid+1]){
-                right = mid;
+            if(nums[mid] < nums[mid+1]){
+                left = mid+1;
             }
             else{
-                left = mid+1;
+                right = mid;
             }
         }
         return left;
