6e53fc0de0720ad93bda24318984f850f66f1bf0
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-04-01 15:08:50 -0700

    8 ms

--- solutions/240. Search a 2D Matrix II.java
+++ solutions/240. Search a 2D Matrix II.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix == null || matrix.length < 1 || matrix[0].length < 1){
-            return false;
-        }
-        int row = 0; 
+        
+        int row = 0;
         int col = matrix[0].length-1;
         
-        //Eliminate columns and rows by starting from the top right corner of the matrix
-        while(col >= 0 && row <= matrix.length-1){
-            if(target == matrix[row][col]){
+        
+        while(col >= 0 && row < matrix.length){
+            
+            if(matrix[row][col] == target){
                 return true;
             }
             else if(target < matrix[row][col]){
