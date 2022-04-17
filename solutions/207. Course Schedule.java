1d645bf6df395dc711ef5f33ca68302895129a99
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-04-16 19:19:53 -0700

    5 ms

--- solutions/207. Course Schedule.java
+++ solutions/207. Course Schedule.java
@@ -1,27 +1,44 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prereq) {
         
-        Map<Integer,ArrayList<Integer>> map = new HashMap();
+        HashMap<Integer, ArrayList<Integer>> courses = new HashMap();
         
         for(int i = 0; i < numCourses; i++){
-            map.put(i, new ArrayList<Integer>());
+            courses.put(i, new ArrayList());
         }
         
-        for(int[] pre : prereq){
-            ArrayList<Integer> list = map.get(pre[0]);
-            list.add(pre[1]);
-            map.put(pre[0], list);
+        for(int i = 0; i < prereq.length; i++){
+            ArrayList<Integer> list = courses.get(prereq[i][0]);
+            list.add(prereq[i][1]);
         }
         
-        Set<Integer> visited = new HashSet();
+        HashSet<Integer> visited = new HashSet();
         
-        for(int[] pre : prereq){
-            if(!helper(pre[0], map, visited)){
+        for(int i = 0; i < prereq.length; i++){
+            if(!helper(prereq[i][0], courses, visited)){
                 return false;
             }
         }
         
         return true;
+        
     }
     
-    public boolean helper(int course, Map<Integer,ArrayList<Integer>> map, Set<Integer> visited){
+    public boolean helper(int currCourse, HashMap<Integer, ArrayList<Integer>> courses, Set<Integer> visited){
+        
+        if(visited.contains(currCourse)){
+            return false;
+        }
+        
+        visited.add(currCourse);
+        ArrayList<Integer> currPreq = courses.get(currCourse);
+        for(int i = 0; i < currPreq.size(); i++){
+            if(!helper(currPreq.get(i), courses, visited)){
+                return false;
+            }
+        }
+        visited.remove(currCourse);
+        courses.put(currCourse, new ArrayList<Integer>());
+        return true;
+    }
+}
