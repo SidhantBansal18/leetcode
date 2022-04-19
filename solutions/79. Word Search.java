dcaa39ad3758431e47f473197e1aa2219b65f6dd
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-04-18 17:52:56 -0700

    133 ms

--- solutions/79. Word Search.java
+++ solutions/79. Word Search.java
@@ -1,38 +1,43 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
         
-        int row = board.length;
-        int col = board[0].length;
+        int rowCount = board.length;
+        int colCount = board[0].length;
+        boolean[][] visited = new boolean[rowCount][colCount];
         
-        for(int i =0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                if(board[i][j] == word.charAt(0) && isWordThere(board, word, i, j, 0)){
-                    return true;
-                }
+        for(int i = 0; i < rowCount; i++){
+            for(int j = 0; j < colCount; j++){
+                
+                if(board[i][j] == word.charAt(0)){
+                    if (bfs(board, word, i, j, 0, visited)){
+                        return true;
+                    }
+                }                
             }
         }
+        
         return false;
     }
     
-    public boolean isWordThere (char[][] board, String word, int row, int col, int index){
+    public boolean bfs(char[][] board, String word, int row, int col, int currWordIndex, boolean[][] visited){
         
-        if(index == word.length()){
+        if(currWordIndex == word.length()){
             return true;
         }
         
-        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != word.charAt(index)){
-            return false;
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col] || board[row][col] != word.charAt(currWordIndex)){
+            return false; 
         }
         
-        char temp = board[row][col];
-        board[row][col] = '*';
         
-        boolean result = isWordThere(board, word, row+1, col, index+1) ||
-            isWordThere(board, word, row-1, col, index+1) || 
-            isWordThere(board, word, row, col+1, index+1) ||
-            isWordThere(board, word, row, col-1, index+1);
+        visited[row][col] = true;
+        boolean result = bfs(board, word, row+1, col, currWordIndex+1, visited)
+            || bfs(board, word, row-1, col, currWordIndex+1, visited)
+            || bfs (board, word, row, col+1, currWordIndex+1, visited)
+            || bfs(board, word, row, col-1, currWordIndex+1, visited);
         
-        board[row][col] = temp;
-        return result;
+        visited[row][col] = false; 
+        return result; 
+        
     }
 }
