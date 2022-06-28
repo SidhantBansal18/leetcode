0d18134761dcb199421b0d6ba97789865765b6bc
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-06-27 21:15:09 -0700

    59 ms

--- solutions/1695. Maximum Erasure Value.java
+++ solutions/1695. Maximum Erasure Value.java
@@ -1,32 +1,22 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-        
-        int left = 0;
-        int globalSum = nums[left];
-        int localSum = nums[left];
         Map<Integer, Integer> indexing = new HashMap();
-        indexing.put(nums[left], 0);
-        int right;
+        int[] prefixSum = new int[nums.length + 1];
         
-        for(right = 1; right < nums.length; right++){
+        int maxScore = 0;
+        
+        for(int left = 0, right = 0; right  < nums.length; right++){
+            prefixSum[right+1] = prefixSum[right] + nums[right];
             
             if(indexing.containsKey(nums[right])){
-                
-                globalSum = Math.max(localSum, globalSum);
-                
-                int value = indexing.get(nums[right]) + 1;
-                while(left < value && left < right){
-                    localSum -= nums[left];
-                    left++;
-                }
-                
+                left = Math.max(left, indexing.get(nums[right]) + 1);
             }
             
-            localSum += nums[right];
+            maxScore = Math.max(maxScore, prefixSum[right+1] - prefixSum[left]);
             
             indexing.put(nums[right], right);
         }
         
-        return Math.max(globalSum, localSum);
+        return maxScore; 
     }
 }
