7e97230c18fd28985e3a76f16e1e20d9d5f67594
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-06-28 20:23:46 -0700

    112 ms

--- solutions/583. Delete Operation for Two Strings.java
+++ solutions/583. Delete Operation for Two Strings.java
@@ -1,24 +1,29 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         
-        int[][] dp = new int[word1.length()+1][word2.length()+1];
+        int[][] memo = new int[word1.length()+1][word2.length()+1];
         
-        for(int row = 0; row <= word1.length(); row++){
-            for(int col = 0; col <= word2.length(); col++){
-                if(row == 0 || col == 0){
-                    continue;
-                }
-                else if(word1.charAt(row-1) == word2.charAt(col-1)){
-                    dp[row][col] = dp[row-1][col-1] + 1;
-                }
-                else{
-                    dp[row][col] = Math.max(dp[row-1][col], dp[row][col-1]);
-                }
-            }
+        return word1.length() + word2.length() - 2 * (lcs(word1, word2, word1.length(), word2.length(), memo));
+    }
+    
+    public int lcs(String word1, String word2, int index1, int index2, int[][] memo){
+        
+        
+        if(index1 == 0 || index2 == 0){
+            return 0;
         }
         
-        int minMoves = (word1.length() + word2.length()) - (2 * dp[word1.length()][word2.length()]);
-        return minMoves;
+        if(memo[index1][index2] > 0){
+            return memo[index1][index2];
+        }
         
+        if(word1.charAt(index1-1) == word2.charAt(index2-1)){
+            memo[index1][index2] = 1 + lcs(word1, word2, index1 - 1, index2 - 1, memo);
+        }
+        
+        else{
+            memo[index1][index2] = Math.max(lcs(word1, word2, index1-1, index2, memo), lcs(word1, word2, index1, index2-1, memo));
+        }
+        return memo[index1][index2];
     }
 }
