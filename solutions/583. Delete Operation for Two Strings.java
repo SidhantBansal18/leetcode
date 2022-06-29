b23f5fabba8f3cc1c4e3c68710e6963a5f8d836d
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-06-28 20:14:26 -0700

    13 ms

--- solutions/583. Delete Operation for Two Strings.java
+++ solutions/583. Delete Operation for Two Strings.java
@@ -1,31 +1,14 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         
-        int[][] dp = new int[word1.length()][word2.length()];
+        int[][] dp = new int[word1.length()+1][word2.length()+1];
         
-        for(int col = 0; col < dp[0].length; col++){
-            if(word1.charAt(0) == word2.charAt(col)){
-                dp[0][col] = 1;
-            }
-            else{
-                dp[0][col] = col != 0? dp[0][col-1]: 0;
-            }
-        }
-        
-        for(int row = 0; row < dp.length; row++){
-            if(word1.charAt(row) == word2.charAt(0)){
-                dp[row][0] = 1;
-            }
-            else{
-                dp[row][0] = row != 0? dp[row-1][0]: 0;
-            }
-        }
-        
-        for(int row = 1; row < dp.length; row++){
-            char firstChar = word1.charAt(row);
-            for(int col = 1; col < dp[0].length; col++){
-                char secondChar = word2.charAt(col);
-                if(firstChar == secondChar){
+        for(int row = 0; row <= word1.length(); row++){
+            for(int col = 0; col <= word2.length(); col++){
+                if(row == 0 || col == 0){
+                    continue;
+                }
+                else if(word1.charAt(row-1) == word2.charAt(col-1)){
                     dp[row][col] = dp[row-1][col-1] + 1;
                 }
                 else{
@@ -34,7 +17,8 @@ class Solution {
             }
         }
         
-        int minMoves = word1.length()+word2.length() - (dp[dp.length-1][dp[0].length-1] * 2);
+        int minMoves = (word1.length() + word2.length()) - (2 * dp[word1.length()][word2.length()]);
         return minMoves;
+        
     }
 }
