73e4bc4f0fb362b06ca31f8ed82be26f5a0e8abc
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-07-07 00:50:09 -0700

    5 ms

--- solutions/135. Candy.java
+++ solutions/135. Candy.java
@@ -1,41 +1,42 @@
 class Solution {
     public int candy(int[] ratings) {
         
-        int[] leftNeighbors = new int[ratings.length];
-        int[] rightNeighbors = new int[ratings.length];
+        int[] neighbors = new int[ratings.length];
         
-        leftNeighbors[0] = 1;
-        rightNeighbors[rightNeighbors.length-1] = 1;
+        neighbors[0] = 1;
         
-        for(int index = 1; index < leftNeighbors.length; index++){
+        for(int index = 1; index < neighbors.length; index++){
             
             if(ratings[index] > ratings[index-1]){
-                leftNeighbors[index] = leftNeighbors[index-1] + 1;
+                neighbors[index] = neighbors[index-1] + 1;
             }
             else{
-                leftNeighbors[index] = 1;
+                neighbors[index] = 1;
             }
             
         }
         
-        for(int index = rightNeighbors.length-2; index >= 0; index--){
+        int minCandies = neighbors[neighbors.length-1];
+        
+        for(int index = neighbors.length-2; index >= 0; index--){
             
             if(ratings[index] > ratings[index+1]){
-                rightNeighbors[index] = rightNeighbors[index+1] + 1;
+                neighbors[index] = Math.max(neighbors[index+1] + 1, neighbors[index]);
             }
-            else{
-                rightNeighbors[index] = 1;
-            }
+            minCandies += neighbors[index];
+            // else{
+            //     neighbors[index] = 1;
+            // }
             
         }
         
-        int minCandies = 0;
-        int[] finalCandies = new int[ratings.length];
+        // int minCandies = 0;
+//         int[] finalCandies = new int[ratings.length];
         
-        for(int i = 0; i < finalCandies.length; i++){
-            finalCandies[i] = Math.max(leftNeighbors[i], rightNeighbors[i]);
-            minCandies += finalCandies[i];
-        }
+//         for(int i = 0; i < finalCandies.length; i++){
+//             finalCandies[i] = Math.max(leftNeighbors[i], rightNeighbors[i]);
+//             minCandies += finalCandies[i];
+//         }
         
         return minCandies;
     }
