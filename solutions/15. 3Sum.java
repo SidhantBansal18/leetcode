cac1cd5bb062277ec55ae24b4b4ec4fc927bbcd9
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-07-12 19:54:59 -0700

    29 ms

--- solutions/15. 3Sum.java
+++ solutions/15. 3Sum.java
@@ -1,31 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList();
+        
         Arrays.sort(nums);
         
+        List<List<Integer>> result = new ArrayList();
+        
         for(int i = 0; i < nums.length; i++){
-            if(i == 0 || i > 0 && nums[i] != nums[i-1]){
-                int sum = 0 - nums[i];
-                int low = i+1;
-                int high = nums.length-1;
-                
-                while(low < high){
-                    if(nums[low] + nums[high] == sum){
-                        result.add(Arrays.asList(nums[low], nums[high], nums[i]));
-                        while(low < high && nums[low] == nums[low+1])   low++;
-                        while(low < high && nums[high] == nums[high-1]) high--;
-                        low++;
-                        high--;
+            
+            int toFind = 0 - nums[i];
+            int start = i+1;
+            int end = nums.length-1;
+            
+            if(i == 0 || (i > 0 && nums[i] != nums[i-1])){
+​
+                while(start < end){
+​
+                    int sum = nums[start] + nums[end];
+                    
+                    if(sum == toFind){
+                        result.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                        while(start < end && nums[start] == nums[start+1])  start++;
+                        while(start < end && nums[end] == nums[end-1])  end--;
                     }
-                    else if(nums[low] + nums[high] > sum){
-                        high--;
+​
+                    if(sum < toFind){
+                        start++;
                     }
                     else{
-                        low++;
+                        end--;
                     }
                 }
             }
         }
-        return result;
+        
+        return result; 
+        
     }
 }
