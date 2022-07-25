9d49fac017659131c2da91df6f3f2697c4caf446
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-07-24 20:52:04 -0700

    15 ms

--- solutions/49. Group Anagrams.java
+++ solutions/49. Group Anagrams.java
@@ -1,32 +1,31 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
+        Map<String, List<String>> map = new HashMap();
+        
+        for(String s : strs){
+            
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            
+            String key = String.valueOf(arr);
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList());
+            }
+            
+            List<String> value = map.get(key);
+            value.add(s);
+            
+            map.put(key, value);
+            
+        }
+        
         List<List<String>> result = new ArrayList();
         
-        Map<String, List<String>> map = new HashMap(); 
-        
-        for(int i = 0; i < strs.length; i++){
-            
-            char[] curr = strs[i].toCharArray();
-            Arrays.sort(curr);
-            String toCheck = String.valueOf(curr);
-            
-            if(!map.containsKey(toCheck)){
-                List<String> list = new ArrayList();
-                list.add(strs[i]);
-                map.put(toCheck, list);
-            }
-            else{
-                List<String> list = map.get(toCheck);
-                list.add(strs[i]);
-                map.put(toCheck, list);
-            }
+        for(Map.Entry<String, List<String>> currValue: map.entrySet()){
+            result.add(currValue.getValue());
         }
         
-        for(Map.Entry<String, List<String>> m : map.entrySet()){
-            result.add(m.getValue());
-        }
-        
-        return result;
+        return result; 
     }
 }
