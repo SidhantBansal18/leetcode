54e2fae981bccc1856b234558d4e33c010aafcea
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-07-31 18:48:59 -0700

    13 ms

--- solutions/56. Merge Intervals.java
+++ solutions/56. Merge Intervals.java
@@ -1,32 +1,33 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         
-        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         
-        List<List<Integer>> result = new ArrayList();
+        List<List<Integer>> result = new ArrayList(); 
         
         result.add(Arrays.asList(intervals[0][0], intervals[0][1]));
-        
-        int current = 0;
+        int curr = 0;
         
         for(int i = 1; i < intervals.length; i++){
             
-            if(intervals[i][0] <= result.get(current).get(1)){
-                result.get(current).set(1, Math.max(intervals[i][1], result.get(current).get(1)));
+            if(intervals[i][0] > result.get(curr).get(1)){
+                result.add(Arrays.asList(intervals[i][0], intervals[i][1]));
+                curr++;
             }
+            
             else{
-                result.add(Arrays.asList(intervals[i][0],intervals[i][1]));
-                current++;
+                result.get(curr).set(0, Math.min(intervals[i][0], result.get(curr).get(0)));
+                result.get(curr).set(1, Math.max(intervals[i][1], result.get(curr).get(1)));
             }
         }
         
-        int[][] resultant = new int[result.size()][2];
+        int[][] resultant = new int[result.size()][result.get(0).size()];
         
         for(int i = 0; i < resultant.length; i++){
             resultant[i][0] = result.get(i).get(0);
             resultant[i][1] = result.get(i).get(1);
         }
         
-        return resultant;
+        return resultant; 
     }
 }
