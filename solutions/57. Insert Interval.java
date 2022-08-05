da1cbb63297fd94e46f9911779e5804af25cb15f
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-08-04 17:43:41 -0700

    1 ms

--- solutions/57. Insert Interval.java
+++ solutions/57. Insert Interval.java
@@ -1,33 +1,26 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         
-        List<List<Integer>> result = new ArrayList();
+        List<int[]> result = new ArrayList();
         
-        int index = 0;
-        
-        while(index < intervals.length && intervals[index][1] < newInterval[0]){
-            result.add(Arrays.asList(intervals[index][0], intervals[index][1]));
-            index++;
+        for(int interval[]: intervals){
+            
+            if(newInterval == null || newInterval[0] > interval[1]){
+                result.add(interval);
+            }
+            
+            else if(interval[0] > newInterval[1]){
+                result.add(newInterval);
+                result.add(interval);
+                newInterval = null;
+            }
+            else{
+                newInterval[0] = Math.min(interval[0], newInterval[0]);
+                newInterval[1] = Math.max(interval[1], newInterval[1]);
+            }
         }
+        if(newInterval != null) result.add(newInterval);
+        return result.toArray(new int[result.size()][]);
         
-        while(index < intervals.length && intervals[index][0] <= newInterval[1]){
-            newInterval[0] = Math.min(intervals[index][0], newInterval[0]);
-            newInterval[1] = Math.max(intervals[index][1], newInterval[1]);
-            index++;
-        }
-        result.add(Arrays.asList(newInterval[0],newInterval[1]));
-        while(index < intervals.length){
-            result.add(Arrays.asList(intervals[index][0],intervals[index][1]));
-            index++;
-        }
-        
-        int[][] finalArr = new int[result.size()][2];
-        
-        for(int i = 0; i < finalArr.length; i++){
-            finalArr[i][0] = result.get(i).get(0);
-            finalArr[i][1] = result.get(i).get(1);
-        }
-        
-        return finalArr;
     }
 }
