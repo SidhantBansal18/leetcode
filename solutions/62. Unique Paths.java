081b846ec521d615944b310b02e05279d02a0974
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-08-06 18:59:15 -0700

    0 ms

--- solutions/62. Unique Paths.java
+++ solutions/62. Unique Paths.java
@@ -5,20 +5,33 @@ class Solution {
         
         int[][] dp = new int[m][n];
         
-        for(int i = 0; i < m-1; i++){
-            dp[i][n-1] = 1;
-        }
-        
-        for(int i = 0; i < n-1; i++){
-            dp[m-1][i] = 1;
-        }
-        
-        for(int i = m-2; i >= 0; i--){
-            for(int j = n-2; j >= 0; j--){
-                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                dp[i][j] = -1;
             }
         }
         
-        return dp[0][0]; 
+        return recursion(dp, m-1, n-1);
+        
+    }
+    
+    public int recursion(int[][] dp, int m, int n){
+        
+        if(m < 0 || n < 0){
+            return 0;
+        }
+        
+        if((m == 1 && n == 0) || (m == 0 && n == 1)){
+            if(dp[m][n] == -1){
+                dp[m][n] = 1;
+            }   
+            return dp[m][n];
+        }
+        
+        if(dp[m][n] == -1){
+            dp[m][n] = recursion(dp, m-1, n) + recursion(dp, m, n-1);
+        }
+        
+        return dp[m][n];
     }
 }
