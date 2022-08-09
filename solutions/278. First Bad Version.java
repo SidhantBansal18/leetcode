e35e14ddefee6158dbdd2027e753225cff49248e
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-08-08 20:17:05 -0700

    22 ms

--- solutions/278. First Bad Version.java
+++ solutions/278. First Bad Version.java
@@ -7,26 +7,18 @@ public class Solution extends VersionControl {
         int left = 1;
         int right = n;
         
-        while(left <= right){
+        while(left < right){
             
             int mid = left + (right-left)/2;
             
             if(isBadVersion(mid)){
-                
-                if(!isBadVersion(mid-1)){
-                    return mid;
-                }
-                
-                else{
-                    right = mid-1;
-                }
+                right = mid;
             }
-            
             else{
                 left = mid+1;
             }
         }
         
-        return -1;
+        return right;
     }
 }
