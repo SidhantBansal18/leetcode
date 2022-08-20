7c98e7312a6f0470918f3a946ce1e06a96861cf7
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-08-20 16:01:29 -0700

    4 ms

--- solutions/11. Container With Most Water.java
+++ solutions/11. Container With Most Water.java
@@ -2,19 +2,18 @@ class Solution {
     public int maxArea(int[] height) {
         
         int maxArea = 0;
-        
         int left = 0;
         int right = height.length-1;
         
         while(left < right){
             
-            int breadth = right - left; 
-            int currArea = Math.min(height[left], height[right]) * breadth;
+            int currArea = Math.min(height[left], height[right]) * (right-left);            
             maxArea = Math.max(currArea, maxArea);
             
             if(height[left] < height[right]){
                 left++;
             }
+            
             else{
                 right--;
             }
