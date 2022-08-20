5a45cf388d8cd00aa45fa22b7912f55a4287eab3
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-08-20 14:51:38 -0700

    1 ms

--- solutions/53. Maximum Subarray.java
+++ solutions/53. Maximum Subarray.java
@@ -2,24 +2,14 @@ class Solution {
     public int maxSubArray(int[] nums) {
         
         int maxSum = nums[0];
-        int currSum = 0;
+        int finalSum = nums[0];
         
-        //If the preceding sum is negative then the sum will start from the current value otherwise update the existing sum
-        
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 1; i < nums.length; i++){
             
-            if(currSum < 0){
-                currSum = nums[i];
-            }
-            else{
-                currSum += nums[i];
-            }
-            
-            
-            maxSum = Math.max(currSum, maxSum);
+            maxSum = Math.max(maxSum+nums[i], nums[i]);
+            finalSum = Math.max(finalSum, maxSum);
         }
         
-        return maxSum; 
-        
+        return finalSum; 
     }
 }
