6b03d3ece3f8931d2f1a5464a75f872ea9d4df4c
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-08-21 02:11:02 -0700

    402 ms

--- solutions/5. Longest Palindromic Substring.java
+++ solutions/5. Longest Palindromic Substring.java
@@ -1,45 +1,50 @@
 class Solution {
     public String longestPalindrome(String s) {
         
-        boolean[][] isPalindrome = new boolean[s.length()][s.length()];
-        int start = 0;
-        int end = 0;
-        int result = Integer.MIN_VALUE;
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int maxLength = 0;
+        int maxRow = 0;
+        int maxCol = 0;
         
-        for(int i = 0; i < isPalindrome.length; i++){
+        for(int i = 0; i <  dp.length; i++){
             
-            for(int j = 0, k = i; k < isPalindrome[0].length; j++, k++){
+            for(int j = 0, k = i ; k < dp[0].length; j++, k++){
                 
-                if(j == k){
-                    isPalindrome[j][k] = true;
+                if(i == 0){
+                    dp[j][k] = true;
                 }
                 
-                else if(k-j == 1){
-                    if(s.charAt(k) == s.charAt(j)){
-                        isPalindrome[j][k] = true;
-                        if((k-j) > result){
-                            result = k-j;
-                            start = j;
-                            end = k;
+                else if(i == 1){
+                    if(s.charAt(j) == s.charAt(k)){
+                        dp[j][k] = true;
+                        int currLength = k-j;
+                        if(currLength > maxLength){
+                            maxRow = j;
+                            maxCol = k;
+                            maxLength = currLength;
                         }
                     }
                 }
                 
                 else{
-                    if(s.charAt(k) == s.charAt(j) && isPalindrome[j+1][k-1]){
-                        isPalindrome[j][k] = true;
-                        if((k-j) > result){
-                            result = k-j;
-                            start = j;
-                            end = k;
+                    
+                    if(s.charAt(j) == s.charAt(k) && dp[j+1][k-1]){
+                        dp[j][k] = true;
+                        int currLength = k-j;
+                        if(currLength > maxLength){
+                            maxRow = j;
+                            maxCol = k;
+                            maxLength = currLength;
                         }
                     }
+                    
                 }
+                
             }
             
         }
         
-        return s.substring(start, end+1).toString();
+        return s.substring(maxRow, maxCol+1).toString();
         
     }
 }
