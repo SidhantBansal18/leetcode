8bb88df7527c574b6e2db49b0df63234bec3b622
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-08-30 23:17:26 -0700

    5 ms

--- solutions/572. Subtree of Another Tree.java
+++ solutions/572. Subtree of Another Tree.java
@@ -16,28 +16,19 @@
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         
-        if(root == null && subRoot == null){
-            return true; 
+        if(root == null || subRoot == null) return false;
+        
+        if(root.val == subRoot.val && check(root, subRoot)){
+            return true;
         }
         
-        if(root == null || subRoot == null){
-            return false; 
-        }
-        
-        boolean result = false;
-        
-        if(root.val == subRoot.val) {
-            result = check(root, subRoot);
-        }
-        
-        return result || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
-        
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
     
     public boolean check(TreeNode root, TreeNode subRoot){
         
         if(root == null && subRoot == null){
-            return true; 
+            return true;
         }
         
         if(root == null || subRoot == null){
@@ -50,5 +41,4 @@ class Solution {
         
         return check(root.left, subRoot.left) && check(root.right, subRoot.right);
     }
-    
 }
