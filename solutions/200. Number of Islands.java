63e3ac77c4797c3cad5a834e41bf98bd5d916ff1
Sidhant Bansal <sidhantbansal1997@gmail.com>
2022-08-31 13:14:33 -0700

    4 ms

--- solutions/200. Number of Islands.java
+++ solutions/200. Number of Islands.java
@@ -3,35 +3,29 @@ class Solution {
         
         int result = 0;
         
-        for(int i = 0; i < grid.length; i++){
+        for(int i = 0; i < grid.length; i++){            
             for(int j = 0; j < grid[0].length; j++){
-                
                 if(grid[i][j] == '1'){
-                    bfs(grid, i, j);
+                    bfs(i, j, grid);
                     result++;
                 }
-                
-            }
+            }            
         }
+        
         return result;
     }
     
-    public void bfs(char[][] grid, int row, int col){
+    public void bfs(int row, int col, char[][] grid){
         
-        if(row < 0 || col < 0 ||row >= grid.length || col >= grid[0].length || grid[row][col] != '1'){
+        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] != '1'){
             return;
         }
         
         grid[row][col] = '0';
-        
-        bfs(grid,row+1,col);
-        bfs(grid,row-1,col);
-        bfs(grid,row,col+1);
-        bfs(grid,row,col-1);
-        
-        return;
+        bfs(row+1, col, grid);
+        bfs(row-1, col, grid);
+        bfs(row, col+1, grid);
+        bfs(row, col-1, grid);
         
     }
-    
-    
 }
